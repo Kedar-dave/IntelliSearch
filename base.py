dbbe5534522810a1b4b696bf6b26bf04b2a11d12
Kedar Dave <kedardave2902@gmail.com>
2024-01-22 14:43:24 +0530

    Commented Video Processor Code

--- base.py
+++ base.py
@@ -1,137 +1,137 @@
 import os
 import bson
-import json
+# import json
 import torch
 import PyPDF2
 import pickle
-from icecream import ic
+# from icecream import ic
 from bson import ObjectId
 from bson.binary import Binary
 from dotenv import load_dotenv
-from collections import Counter
+# from collections import Counter
 from multipledispatch import dispatch
 from torch.multiprocessing import Pool
 from txtai.embeddings import Embeddings
-from moviepy.editor import VideoFileClip
+# from moviepy.editor import VideoFileClip
 # from db import MongoDB
 from util import file_finder, text_cleaning, transform_text
-from deepgram import DeepgramClientOptions, DeepgramClient, PrerecordedOptions, FileSource
+# from deepgram import DeepgramClientOptions, DeepgramClient, PrerecordedOptions, FileSource
 # import streamlit as st
 __author__ = "Kedar"
 __version__ = "0.1"
 
 load_dotenv()
 torch.device("cuda:0")
-class VideoProcessor:
-    def __init__(self):
-        self.videoData: list = list()
-        DEEPGRAM_API_KEY = os.environ.get("DG_API_KEY")
-        config: DeepgramClientOptions = DeepgramClientOptions(verbose=False)
-        self.deepgram: DeepgramClient = DeepgramClient(
-            DEEPGRAM_API_KEY, config)
-        self.options = PrerecordedOptions(
-            model="nova-2",
-            smart_format=True,
-            utterances=True,
-            punctuate=True,
-            diarize=True)
-        self.KEYS = [
-            "transaction_key", "request_id", "sha256", "created",
-            "models", "warnings", "model_info", "summary_info", "words",
-            "search", "confidence", "punctuated_word", "speaker", "speaker_confidence", "summaries",
-            "entities", "translations", "topics", "detected_language",
-            "utterances", "summary"]
-    def __del__(self):
-        try:
-            os.remove("temp.mp3")
-        except:
-            pass
+# class VideoProcessor:
+#     def __init__(self):
+#         self.videoData: list = list()
+#         DEEPGRAM_API_KEY = os.environ.get("DG_API_KEY")
+#         config: DeepgramClientOptions = DeepgramClientOptions(verbose=False)
+#         self.deepgram: DeepgramClient = DeepgramClient(
+#             DEEPGRAM_API_KEY, config)
+#         self.options = PrerecordedOptions(
+#             model="nova-2",
+#             smart_format=True,
+#             utterances=True,
+#             punctuate=True,
+#             diarize=True)
+#         self.KEYS = [
+#             "transaction_key", "request_id", "sha256", "created",
+#             "models", "warnings", "model_info", "summary_info", "words",
+#             "search", "confidence", "punctuated_word", "speaker", "speaker_confidence", "summaries",
+#             "entities", "translations", "topics", "detected_language",
+#             "utterances", "summary"]
+#     def __del__(self):
+#         try:
+#             os.remove("temp.mp3")
+#         except:
+#             pass
 
-    # @st.cache_data streamlitcode
-    def get_video_data(self) -> list:
-        return self.videoData
+#     # @st.cache_data streamlitcode
+#     def get_video_data(self) -> list:
+#         return self.videoData
 
-    def __set_video_data(self, name: str, transcription: str,
-                         keywords: list,
-                         duration: float) -> None:
-        docDict = {
-            "type": "video",
-            "name": name,
-            "transcription": transcription,
-            "words": dict(Counter(keywords)),
-            "duration": duration
-        }
-        self.videoData.append(docDict)
+#     def __set_video_data(self, name: str, transcription: str,
+#                          keywords: list,
+#                          duration: float) -> None:
+#         docDict = {
+#             "type": "video",
+#             "name": name,
+#             "transcription": transcription,
+#             "words": dict(Counter(keywords)),
+#             "duration": duration
+#         }
+#         self.videoData.append(docDict)
 
-    def video_to_text_batch(self, videoFolderPath: str) -> bool:
-        # convertbar = st.progress( streamlitcode
-        #     0, text="Converting Videos to text. Please Wait...") streamlitcode
-        videoFiles = file_finder(
-            videoFolderPath, (".mp4", ".mov", ".mkv", ".avi"))
-        # counter = 0 streamlitcode
-        # numOfFiles = len(videoFiles) streamlitcode
-        for videoName in videoFiles:
-            self.video_to_text(videoName,videoFolderPath)
-        #     counter += 1 streamlitcode
-        #     status = counter/numOfFiles streamlitcode
-        #     convertbar.progress( streamlitcode
-        #         status, text="Converting Videos to text. Please Wait...") streamlitcode
-        # convertbar.empty() streamlitcode
+#     def video_to_text_batch(self, videoFolderPath: str) -> bool:
+#         # convertbar = st.progress( streamlitcode
+#         #     0, text="Converting Videos to text. Please Wait...") streamlitcode
+#         videoFiles = file_finder(
+#             videoFolderPath, (".mp4", ".mov", ".mkv", ".avi"))
+#         # counter = 0 streamlitcode
+#         # numOfFiles = len(videoFiles) streamlitcode
+#         for videoName in videoFiles:
+#             self.video_to_text(videoName,videoFolderPath)
+#         #     counter += 1 streamlitcode
+#         #     status = counter/numOfFiles streamlitcode
+#         #     convertbar.progress( streamlitcode
+#         #         status, text="Converting Videos to text. Please Wait...") streamlitcode
+#         # convertbar.empty() streamlitcode
 
-    def video_to_text(self, videoName:str,videoFolderPath:str="../data") -> bool:
-        videoPath = os.path.normpath(os.path.join(videoFolderPath, videoName))
-        ic(videoPath)
+#     def video_to_text(self, videoName:str,videoFolderPath:str="../data") -> bool:
+#         videoPath = os.path.normpath(os.path.join(videoFolderPath, videoName))
+#         ic(videoPath)
 
-        with VideoFileClip(videoPath) as video:
-            audioClip = video.audio
-            audioClip.write_audiofile("temp.mp3",codec="mp3", verbose=False, logger=None)
-            transcript = self.__transcribe("temp.mp3")
-            keywords = transform_text(transcript["transcript"])
-            self.__set_video_data(
-                videoName, transcript["transcript"], keywords, transcript["duration"])
+#         with VideoFileClip(videoPath) as video:
+#             audioClip = video.audio
+#             audioClip.write_audiofile("temp.mp3",codec="mp3", verbose=False, logger=None)
+#             transcript = self.__transcribe("temp.mp3")
+#             keywords = transform_text(transcript["transcript"])
+#             self.__set_video_data(
+#                 videoName, transcript["transcript"], keywords, transcript["duration"])
         
 
-    def __transcribe(self, audioFile: str) -> dict:
+#     def __transcribe(self, audioFile: str) -> dict:
 
-        with open(audioFile, "rb") as file:
-            bufferData = file.read()
+#         with open(audioFile, "rb") as file:
+#             bufferData = file.read()
 
-        payload: FileSource = {"buffer": bufferData}
+#         payload: FileSource = {"buffer": bufferData}
 
-        fileResponse = self.deepgram.listen.prerecorded.v(
-            "1").transcribe_file(payload, self.options)
+#         fileResponse = self.deepgram.listen.prerecorded.v(
+#             "1").transcribe_file(payload, self.options)
 
-        res = fileResponse.to_json()
-        res = json.loads(res)
-        self.__purge_keys(res)
-        response = self.__fmt(res)
-        return response
+#         res = fileResponse.to_json()
+#         res = json.loads(res)
+#         self.__purge_keys(res)
+#         response = self.__fmt(res)
+#         return response
 
-    def __purge_keys(self, data: dict):
+#     def __purge_keys(self, data: dict):
 
-        if isinstance(data, dict):
-            for key in list(data.keys()):
-                if key == 'channels' and isinstance(data[key], int):
-                    del data[key]
-                elif key in self.KEYS:
-                    del data[key]
-                else:
-                    self.__purge_keys(data[key])
-        elif isinstance(data, list):
-            for item in data:
-                self.__purge_keys(item)
+#         if isinstance(data, dict):
+#             for key in list(data.keys()):
+#                 if key == 'channels' and isinstance(data[key], int):
+#                     del data[key]
+#                 elif key in self.KEYS:
+#                     del data[key]
+#                 else:
+#                     self.__purge_keys(data[key])
+#         elif isinstance(data, list):
+#             for item in data:
+#                 self.__purge_keys(item)
 
-    def __fmt(self, data: dict) -> dict:
+#     def __fmt(self, data: dict) -> dict:
 
-        duration = data.get("metadata", {}).get("duration", 0)
-        transcript = data.get("results", {}).get("channels", [{}])[
-            0].get("alternatives", [{}])[0].get("transcript", "")
-        transcript = transcript.replace('\n', ' ')
-        response = {
-            "duration": duration,
-            "transcript": transcript,
-        }
-        return response
+#         duration = data.get("metadata", {}).get("duration", 0)
+#         transcript = data.get("results", {}).get("channels", [{}])[
+#             0].get("alternatives", [{}])[0].get("transcript", "")
+#         transcript = transcript.replace('\n', ' ')
+#         response = {
+#             "duration": duration,
+#             "transcript": transcript,
+#         }
+#         return response
     
 class DocumentProcessor:
 
